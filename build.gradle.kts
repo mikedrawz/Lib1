f052a6eadd35482d8a0531fe7f29006a9aae9418
mike.drawz <michael.drawz@outlook.com>
2023-03-29 10:29:32 -0500

    adding missing gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+@Suppress("DSL_SCOPE_VIOLATION") // is needed to avoid a known false positive
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+	alias(libs.plugins.android.library)
+    `maven-publish`
+}
+
+
+group = "com.example.mdrawz"
+version = project.property("version")!!
+
+// *************************************************************************************************************
+// Dependency repositories
+// *************************************************************************************************************
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+kotlin {
+	jvm {
+		compilations.all {
+			kotlinOptions.jvmTarget = libs.versions.jvm.target.get()
+		}
+		testRuns["test"].executionTask.configure {
+			useJUnitPlatform()
+		}
+	}
+
+	android {
+		compilations.all {
+			kotlinOptions {
+				jvmTarget = libs.versions.jvm.target.get()
+			}
+		}
+		publishLibraryVariants("release")
+	}
+
+	sourceSets {
+		val commonMain by getting {
+			dependencies {
+			}
+		}
+		val commonTest by getting {
+			dependencies {
+				implementation(kotlin("test"))
+			}
+		}
+		val jvmMain by getting
+		val jvmTest by getting
+		val androidMain by getting
+		val androidTest by getting
+	}
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+rootProject.name = "Lib1"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+
